9ec6c60998625769d33dfb2e8af9aefebe58041a
kbtomic <ktomic01@fesb.hr>
2020-11-16 20:59:53 +0100

    Changed line for first commit

---
+++ Internship-1/Internship-1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Internship_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Text for first commit!");
+        }
+    }
+}
